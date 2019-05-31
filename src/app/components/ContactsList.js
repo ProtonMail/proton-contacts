886d8cbd90d163642a76970406300ad0ab6ebc38
Richard <richard@protonmail.com>
2019-05-31 09:11:47 +0200

    Display contact group in contact list

--- src/app/components/ContactsList.js
+++ src/app/components/ContactsList.js
@@ -1,27 +1,54 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Checkbox } from 'react-components';
-import { Link } from 'react-router-dom';
+import { Checkbox, Icon, useContactGroups } from 'react-components';
+import { Redirect } from 'react-router-dom';
+import { addPlus } from 'proton-shared/lib/helpers/string';
 
 const ContactsList = ({ contacts, onCheck }) => {
+    const [contactGroups] = useContactGroups();
+    const mapContactGroups = contactGroups.reduce((acc, contactGroup) => {
+        acc[contactGroup.ID] = contactGroup;
+        return acc;
+    }, Object.create(null));
+
     const handleCheck = ({ target }) => {
         const contactID = target.getAttribute('data-contact-id');
         onCheck(contactID, target.checked);
     };
 
+    const handleClick = (ID) => () => {
+        <Redirect to={`/contacts/${ID}`} />;
+    };
+
     return (
         <div>
             <ul className="unstyled">
-                {contacts.map(({ ID, Name, emails, isChecked }) => {
+                {contacts.map(({ ID, Name, LabelIDs = [], emails, isChecked }) => {
                     return (
-                        <li key={ID} className="p1 border-bottom flex">
+                        <li key={ID} className="p1 border-bottom flex bg-global-white" onClick={handleClick(ID)}>
                             <div>
                                 <Checkbox checked={isChecked} onChange={handleCheck} data-contact-id={ID} />
                             </div>
                             <div>
-                                <Link to={`/contacts/${ID}`}>
-                                    {Name} {emails.join(', ')}
-                                </Link>
+                                <div className="flex flex-spacebetween">
+                                    <span className="bold">{Name}</span>
+                                    {LabelIDs.length ? (
+                                        <div>
+                                            {LabelIDs.map((labelID) => {
+                                                const { Color, Name } = mapContactGroups[labelID];
+                                                return (
+                                                    <Icon
+                                                        key={labelID}
+                                                        name="contacts-groups"
+                                                        color={Color}
+                                                        title={Name}
+                                                    />
+                                                );
+                                            })}
+                                        </div>
+                                    ) : null}
+                                </div>
+                                <div title={emails.join(', ')}>{addPlus(emails)}</div>
                             </div>
                         </li>
                     );
