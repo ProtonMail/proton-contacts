90cfac71d1ad34886d3e5e680eb1e4719656b890
Richard <richard@protonmail.com>
2019-05-31 08:55:14 +0200

    Add contact placeholder

--- src/app/components/ContactPlaceholder.js
+++ src/app/components/ContactPlaceholder.js
@@ -1,5 +1,58 @@
+import React from 'react';
+import { c } from 'ttag';
+import { PrimaryButton } from 'react-components';
+import { Link } from 'react-router-dom';
+
 const ContactPlaceholder = () => {
-    return 'ContactPlaceholder';
+    const handleImport = () => {};
+    const handleExport = () => {};
+
+    return (
+        <div className="p2">
+            <div className="aligncenter">
+                <h1>{c('Title').t`Contacts`}</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at tristique mauris, non eleifend
+                    orci. Etiam pharetra consequat tristique. Sed tristique ipsum quis suscipit ullamcorper.
+                </p>
+            </div>
+            <div className="flex-autogrid">
+                <div className="flex-autogrid-item">
+                    <div className="p1 aligncenter bordered-container">
+                        <div className="bold">{c('Title').t`Import contacts`}</div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at tristique mauris, non
+                            eleifend orci. Etiam pharetra consequat tristique. Sed tristique ipsum quis suscipit
+                            ullamcorper.
+                        </p>
+                        <PrimaryButton onClick={handleImport}>{c('Action').t`Import`}</PrimaryButton>
+                    </div>
+                </div>
+                <div className="flex-autogrid-item">
+                    <div className="p1 aligncenter bordered-container">
+                        <div className="bold">{c('Title').t`Export contacts`}</div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at tristique mauris, non
+                            eleifend orci. Etiam pharetra consequat tristique. Sed tristique ipsum quis suscipit
+                            ullamcorper.
+                        </p>
+                        <PrimaryButton onClick={handleExport}>{c('Action').t`Export`}</PrimaryButton>
+                    </div>
+                </div>
+                <div className="flex-autogrid-item">
+                    <div className="p1 aligncenter bordered-container">
+                        <div className="bold">{c('Title').t`Contacts settings`}</div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean at tristique mauris, non
+                            eleifend orci. Etiam pharetra consequat tristique. Sed tristique ipsum quis suscipit
+                            ullamcorper.
+                        </p>
+                        <Link to="/settings/account">{c('Action').t`Settings`}</Link>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default ContactPlaceholder;
--- src/app/content/AuthLayout.js
+++ src/app/content/AuthLayout.js
@@ -1,13 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Route } from 'react-router';
+import { Sidebar, useContactGroups, Loader } from 'react-components';
 
 import AuthHeader from './AuthHeader';
 
 const AuthLayout = ({ children }) => {
+    const [contactGroups, loading] = useContactGroups();
+
+    if (loading) {
+        return <Loader />;
+    }
+
+    const list = contactGroups.map(({ Name, Color, ID }) => ({
+        icon: 'contacts-groups',
+        color: Color,
+        text: Name,
+        link: `/contacts/group/${ID}`
+    }));
+
     return (
         <>
             <AuthHeader />
             <div className="flex flex-nowrap">
+                <Route path="/:path" render={() => <Sidebar list={list} />} />
                 <div className="main flex-item-fluid main-area">
                     <div className="flex flex-nowrap">{children}</div>
                 </div>
