a8f7b85032dc4d8eaf9ce9431fd3fb18311b14d8
edu <econdepe@protonmail.com>
2019-06-20 17:53:36 +0200

    first version

--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -2,14 +2,29 @@ import React, { useRef, useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
 import { Loader, GenericError } from 'react-components';
 
-import useContact from '../hooks/useContact';
-import { prepareContact, bothUserKeys } from '../helpers/decrypt';
+import { prepareContact, bothUserKeys } from '../helpers/cryptoTools';
 import ContactView from './ContactView';
 
 const Contact = ({ contactID, userKeysList }) => {
     const [model, setModel] = useState({ ID: contactID });
     const ref = useRef(contactID);
     const [contact, contactLoading, contactFetchError] = useContact(contactID);
+
+    const request = async () => {
+        try {
+            setLoading(true);
+            const { Contact } = await api(getContact(contactID));
+            // console.log(Contact);
+            const { properties, errors } = await prepareContact(Contact, { publicKeys, privateKeys });
+            // console.log(properties);
+            setModel({ properties, errors });
+            setLoading(false);
+        } catch (error) {
+            setLoading(false);
+            setModel({ ...model, errors: [FAIL_TO_LOAD] });
+            throw error;
+        }
+    };
 
     useEffect(() => {
         if (contact && Array.isArray(userKeysList)) {
