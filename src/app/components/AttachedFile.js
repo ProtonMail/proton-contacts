e8e16db336bb11afadfc75048c638c4d00f8d2bc
edu <econdepe@protonmail.com>
2019-07-03 18:33:49 +0200

    allow only one import file

---
+++ src/app/components/AttachedFile.js
@@ -0,0 +1,46 @@
+import React, { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { c } from 'ttag';
+import { Icon, Button } from 'react-components';
+
+import humanSize from 'proton-shared/lib/helpers/humanSize';
+
+const splitExtension = (filename) => {
+    if (!/\./.test(filename)) {
+        return [filename, ''];
+    }
+    const ext = filename.split('.').pop();
+    const name = filename.substring(0, filename.length - ext.length - 1);
+
+    return [name, ext];
+};
+
+const AttachedFile = ({ file, iconName, className, clear, onClear, ...rest }) => {
+    return (
+        <div className={`flex w100 ${className}`} {...rest}>
+            <div className="bordered-container p0-5 mb1 flex flex-spacebetween w10">
+                <Icon name={iconName} />
+            </div>
+            <div className="bordered-container p0-5 mb1 flex flex-spacebetween w90">
+                <div>
+                    <div>{splitExtension(file.name)[0]}</div>
+                    <div>{`${splitExtension(file.name)[1].toUpperCase()} - ${humanSize(file.size)}`}</div>
+                </div>
+                <Button onClick={onClear}>{clear}</Button>
+            </div>
+        </div>
+    );
+};
+
+AttachedFile.propTypes = {
+    file: PropTypes.shape({ name: PropTypes.string, size: PropTypes.number }).isRequired,
+    iconName: PropTypes.string.isRequired,
+    clear: PropTypes.string,
+    onClear: PropTypes.func
+};
+
+AttachedFile.defaultProps = {
+    clear: c('Action').t`Delete`
+};
+
+export default AttachedFile;
--- src/app/components/Contact.js
+++ src/app/components/Contact.js
@@ -15,7 +15,6 @@ const Contact = ({ contactID, userKeysList }) => {
             setLoading(true);
             const { Contact } = await api(getContact(contactID));
             const { properties, errors } = await prepareContact(Contact, { publicKeys, privateKeys });
-            debugger;
             setModel({ properties, errors });
             setLoading(false);
         } catch (error) {
