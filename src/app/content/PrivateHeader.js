1357aa3794c27538d2216b0e7bb4bfed4b5b10bb
Richard <richard@protonmail.com>
2019-08-06 09:42:11 +0200

    Review header

--- src/app/content/PrivateHeader.js
+++ src/app/content/PrivateHeader.js
@@ -4,8 +4,10 @@ import PropTypes from 'prop-types';
 import { SearchInput, MainLogo, Icon } from 'react-components';
 import { APPS } from 'proton-shared/lib/constants';
 import { Link } from 'react-router-dom';
+import { withRouter } from 'react-router';
 
-const PrivateHeader = ({ search, onSearch }) => {
+const PrivateHeader = ({ search, onSearch, location }) => {
+    const inSettings = location.pathname.startsWith('/contacts/settings');
     return (
         <header className="header flex flex-nowrap reset4print">
             <MainLogo currentApp={APPS.PROTONCONTACTS} url="/contacts" />
@@ -24,15 +26,20 @@ const PrivateHeader = ({ search, onSearch }) => {
             <div className="topnav-container flex-item-centered-vert flex-item-fluid">
                 <ul className="topnav-list unstyled mt0 mb0 ml1 flex flex-nowrap">
                     <li className="mr1">
-                        <Link to="/contacts" className="topnav-link inline-flex flex-nowrap nodecoration rounded">
+                        <Link
+                            to="/contacts"
+                            className="topnav-link inline-flex flex-nowrap nodecoration rounded"
+                            aria-current={!inSettings}
+                        >
                             <Icon name="contacts" className="topnav-icon mr0-5 flex-item-centered-vert fill-white" />
                             {c('Title').t`Contacts`}
                         </Link>
                     </li>
                     <li className="mr1">
                         <Link
-                            to="contacts/settings"
+                            to="/contacts/settings"
                             className="topnav-link inline-flex flex-nowrap nodecoration rounded"
+                            aria-current={inSettings}
                         >
                             <Icon
                                 name="settings-master"
@@ -49,7 +56,8 @@ const PrivateHeader = ({ search, onSearch }) => {
 
 PrivateHeader.propTypes = {
     search: PropTypes.string,
-    onSearch: PropTypes.func
+    onSearch: PropTypes.func,
+    location: PropTypes.object
 };
 
-export default PrivateHeader;
+export default withRouter(PrivateHeader);
