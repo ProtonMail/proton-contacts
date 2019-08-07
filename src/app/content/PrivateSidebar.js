e2cbb37489239f5dc3d4381ab79a621cc1c632ac
edu <econdepe@protonmail.com>
2019-08-07 11:08:52 +0200

    fix issue with rebase

--- src/app/content/PrivateSidebar.js
+++ src/app/content/PrivateSidebar.js
@@ -5,9 +5,6 @@ import PropTypes from 'prop-types';
 
 import ContactModal from '../components/ContactModal';
 
-import ContactGroupModal from '../components/ContactGroupModal';
-import ContactGroupsModal from '../components/ContactGroupsModal';
-
 const PrivateSidebar = ({ contactGroups }) => {
     const { createModal } = useModals();
 
