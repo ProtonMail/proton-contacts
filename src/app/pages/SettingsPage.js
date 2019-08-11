ae7021c509cf343b70633340778b319c3ca50282
mmso <mmso@mmso.se>
2019-08-11 22:16:24 +0200

    Fix import

--- src/app/pages/SettingsPage.js
+++ src/app/pages/SettingsPage.js
@@ -1,12 +1,12 @@
 import React from 'react';
-import { useUser } from 'react';
+import { useUser } from 'react-components';
 
 import ContactsSection from '../components/ContactsSection';
 import ContactGroupsSection from '../components/ContactGroupsSection';
 
 const SettingsPage = () => {
     const [{ hasPaidMail }] = useUser();
-    
+
     return (
         <main className="main-area-content bg-white relative flex-item-fluid p2">
             <ContactsSection />
