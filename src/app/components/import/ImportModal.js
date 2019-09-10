e05bb7a694c2a6e5684ecd02cfcd975c5635f710
edu <econdepe@protonmail.com>
2019-09-10 17:47:04 +0200

    remove cancel button after finish

--- src/app/components/import/ImportModal.js
+++ src/app/components/import/ImportModal.js
@@ -161,7 +161,7 @@ const ImportModal = ({ userKeysList, ...rest }) => {
             };
             const footer = (
                 <>
-                    <ResetButton>{c('Action').t`Cancel`}</ResetButton>
+                    {!importFinished && <ResetButton>{c('Action').t`Cancel`}</ResetButton>}
                     <PrimaryButton loading={!importFinished} type="submit">
                         {c('Action').t`Close`}
                     </PrimaryButton>
