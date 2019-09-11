206c0cdf9cb40c20cb88e8ecbeed6acfbfefcb91
edu <econdepe@protonmail.com>
2019-09-11 13:06:57 +0200

    improve code for progress

--- src/app/helpers/progress.js
+++ src/app/helpers/progress.js
@@ -12,6 +12,39 @@ export const percentageProgress = (successful, failed, total) => {
         // assume the process has not started
         return 0;
     }
-    // in case something goes south, do not allow progress > 100
-    return Math.min(Math.floor(((+done + notDone) / total) * 100), 100);
+    // in case successful + failed > total, do not allow progress > 100
+    return Math.min(Math.floor(((+successful + failed) / total) * 100), 100);
+};
+
+/**
+ * Combine progresses of several processes with predefined allocation percentages
+ * @param {Array} processes                 Processes to be combined. Format: { allocated, successful, failed, total}
+ * @param {Number} processes.allocated      Allocated percentage for a process. E.g. 0.25
+ *
+ * @return {Number}                         Combined progrees
+ */
+export const combineProgress = (processes = []) => {
+    const { combinedTotal, combinedAllocations, progresses } = processes.reduce(
+        (acc, { allocated, successful, failed, total }) => {
+            acc.combinedTotal += total;
+            acc.combinedAllocations += allocated;
+            acc.progresses.push(percentageProgress(successful, failed, total));
+            return acc;
+        },
+        { combinedTotal: 0, combinedAllocations: 0, progresses: [] }
+    );
+    if (combinedAllocations !== 1 && !!processes.length) {
+        throw new Error('Allocations must add up to one');
+    }
+    if (!combinedTotal) {
+        return 0;
+    }
+    const combinedProgress = processes.reduce((acc, { allocated, total }, i) => {
+        // set progress to 100 if there are no tasks to be performed for this process,
+        // but there are tasks in other processes
+        const progress = allocated * (!total && !!combinedTotal ? 100 : progresses[i]);
+        return acc + progress;
+    }, 0);
+
+    return Math.round(combinedProgress);
 };
