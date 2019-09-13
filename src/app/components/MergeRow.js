d0530f5307b37d44f79519e6086da108788bdadc
edu <econdepe@protonmail.com>
2019-09-13 18:50:40 +0200

    move MergeRow

--- src/app/components/MergeRow.js
+++ src/app/components/MergeRow.js
@@ -4,16 +4,16 @@ import { c } from 'ttag';
 import { Icon } from 'react-components';
 
 const MergeRow = ({ style, onMerge, ...rest }) => {
+    const boldText = <b key="boldface">{c('Info').t`Two or more contacts appear to be identical`}</b>;
+
     return (
         <div style={style} className="p1 flex flex-nowrap flex-items-center bg-pm-blue color-white" {...rest}>
             <div className="mr1">
                 <Icon name="merge" color="white" />
             </div>
             <div className="flex flex-column">
-                <div className="flex flex-items-center">
-                    <span className="bold">{c('Info').t`Two or more contacts appear to be identical.`}</span>
-                    <span className="mr0-5">{c('Info')
-                        .jt`<b key="boldface">Two or more contacts appear to be identical.</b>Do you want to merge these contacts now?`}</span>
+                <div>
+                    <span className="mr0-5">{c('Info').jt`${boldText}. Do you want to merge these contacts now?`}</span>
                     <button type="button" className="color-white underline" onClick={onMerge}>
                         {c('Action').t`Merge`}
                     </button>
