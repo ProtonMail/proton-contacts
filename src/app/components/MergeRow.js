49cf826570555c7d65f05aaf1033d3ea783cabf9
edu <econdepe@protonmail.com>
2019-09-17 16:42:32 +0200

    improve button

--- src/app/components/MergeRow.js
+++ src/app/components/MergeRow.js
@@ -1,26 +1,25 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { c } from 'ttag';
-import { Icon } from 'react-components';
+import { Icon, Button } from 'react-components';
 
 const MergeRow = ({ loadingUserKeys, style, onMerge, ...rest }) => {
     const boldText = <b key="boldface">{c('Info').t`Two or more contacts appear to be identical.`}</b>;
 
     return (
-        <div style={style} className="p1 flex flex-nowrap flex-items-center bg-pm-blue color-white" {...rest}>
+        <div style={style} className="p1 flex flex-nowrap flex-items-center bg-primary color-white" {...rest}>
             <div className="mr1 flex-item-noshrink">
                 <Icon name="merge" color="white" />
             </div>
             <div className="alignleft">
                 <span className="mr0-5">{c('Info').jt`${boldText} Do you want to merge these contacts now?`}</span>
-                <button
-                    type="button"
-                    className="color-white underline alignbaseline"
+                <Button
+                    className="pm-button--small pm-button--primary pm-button--whiteborder alignbaseline"
                     disabled={loadingUserKeys}
                     onClick={onMerge}
                 >
                     {c('Action').t`Merge`}
-                </button>
+                </Button>
             </div>
         </div>
     );
