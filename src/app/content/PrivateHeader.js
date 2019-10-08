47370b28aaa8b5827dd216b6b321ffc4bc44a9f8
edu san <33841139+econdepe@users.noreply.github.com>
2019-10-08 09:11:18 +0200

    Changes to top navigation bar (#175)
    * fixes to topnavbar
    
    * relocate upgrade button
    
    * Review

--- src/app/content/PrivateHeader.js
+++ src/app/content/PrivateHeader.js
@@ -1,68 +1,40 @@
 import React from 'react';
 import { c } from 'ttag';
 import PropTypes from 'prop-types';
-import { SearchInput, MainLogo, Icon, UserDropdown, SupportDropdown, UpgradeButton, useUser } from 'react-components';
-import { Link } from 'react-router-dom';
+import {
+    MainLogo,
+    Searchbox,
+    SupportDropdown,
+    UpgradeButton,
+    useUser,
+    TopNavbar,
+    TopNavbarLink
+} from 'react-components';
 import { withRouter } from 'react-router';
 
 const PrivateHeader = ({ search, onSearch, location }) => {
-    const [{ isFree }] = useUser();
+    const [{ hasPaidMail }] = useUser();
     const inSettings = location.pathname.startsWith('/contacts/settings');
     return (
         <header className="header flex flex-nowrap reset4print">
             <MainLogo url="/contacts" />
-            <div className="searchbox-container relative flex-item-centered-vert">
-                {inSettings ? null : (
-                    <label htmlFor="global_search">
-                        <span className="sr-only">{c('Placeholder').t`Search`}</span>
-                        <SearchInput
-                            value={search}
-                            onChange={onSearch}
-                            id="global_search"
-                            placeholder={c('Placeholder').t`Search`}
-                            className="searchbox-field"
-                        />
-                    </label>
-                )}
-            </div>
-            <div className="topnav-container flex-item-centered-vert flex-item-fluid">
-                <ul className="topnav-list unstyled mt0 mb0 ml1 flex flex-nowrap">
-                    <li className="mr1">
-                        <Link
-                            to="/contacts"
-                            className="topnav-link inline-flex flex-nowrap nodecoration rounded"
-                            aria-current={!inSettings}
-                        >
-                            <Icon name="contacts" className="topnav-icon mr0-5 flex-item-centered-vert fill-white" />
-                            <span className="navigation-title topnav-linkText">{c('Title').t`Contacts`}</span>
-                        </Link>
-                    </li>
-                    <li className="mr1">
-                        <Link
-                            to="/contacts/settings"
-                            className="topnav-link inline-flex flex-nowrap nodecoration rounded"
-                            aria-current={inSettings}
-                        >
-                            <Icon
-                                name="settings-master"
-                                className="topnav-icon mr0-5 flex-item-centered-vert fill-white"
-                            />
-                            <span className="navigation-title topnav-linkText">{c('Title').t`Settings`}</span>
-                        </Link>
-                    </li>
-                    {isFree ? (
-                        <li className="mr1">
-                            <UpgradeButton className="topnav-link inline-flex flex-nowrap nodecoration rounded" />
-                        </li>
-                    ) : null}
-                    <li className="mr1">
-                        <SupportDropdown />
-                    </li>
-                    <li className="mlauto mtauto mbauto relative flex-item-noshrink">
-                        <UserDropdown />
-                    </li>
-                </ul>
-            </div>
+            {!inSettings && <Searchbox placeholder={c('Placeholder').t`Search`} value={search} onChange={onSearch} />}
+            <TopNavbar>
+                {hasPaidMail ? null : <UpgradeButton external={true} />}
+                <TopNavbarLink
+                    to="/contacts"
+                    icon="contacts"
+                    text={c('Title').t`Contacts`}
+                    aria-current={!inSettings}
+                />
+                <TopNavbarLink
+                    to="/contacts/settings"
+                    icon="settings-master"
+                    text={c('Title').t`Settings`}
+                    aria-current={inSettings}
+                />
+                <SupportDropdown />
+            </TopNavbar>
         </header>
     );
 };
