08477b83707118d4c51c6b320a14387ac8db05c2
Richard <richard@protonmail.com>
2019-10-08 10:19:52 +0200

    Review contact settings structure

--- src/app/pages/SettingsPage.js
+++ src/app/pages/SettingsPage.js
@@ -1,18 +1,19 @@
 import React from 'react';
-import { useUser } from 'react-components';
+import PropTypes from 'prop-types';
+import { SettingsTitle } from 'react-components';
 
-import ContactsSection from '../components/ContactsSection';
-import ContactGroupsSection from '../components/ContactGroupsSection';
-
-const SettingsPage = () => {
-    const [{ hasPaidMail }] = useUser();
-
+const SettingsPage = ({ title, children }) => {
     return (
-        <main className="main-area-content bg-white relative flex-item-fluid p2">
-            <ContactsSection />
-            {hasPaidMail ? <ContactGroupsSection /> : null}
+        <main className="main-area-content bg-white relative flex-item-fluid">
+            <SettingsTitle>{title}</SettingsTitle>
+            <div className="container-section-sticky">{children}</div>
         </main>
     );
+};
+
+SettingsPage.propTypes = {
+    title: PropTypes.string.isRequired,
+    children: PropTypes.node.isRequired
 };
 
 export default SettingsPage;
