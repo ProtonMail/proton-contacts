b7c996f2955c9f82e2a12d4a4bb4b575e4fb04ac
edu <econdepe@protonmail.com>
2019-10-14 13:17:29 +0200

    update contact view after import

--- src/app/components/import/ImportModal.js
+++ src/app/components/import/ImportModal.js
@@ -154,6 +154,10 @@ const ImportModal = ({ userKeysList, ...rest }) => {
                 </PrimaryButton>
             );
 
+            const handleSubmit = async () => {
+                await call();
+                rest.onClose();
+            };
             const handleFinish = async () => {
                 // temporarily disabled
                 // if (hasCategories(vcardContacts)) {
@@ -175,7 +179,7 @@ const ImportModal = ({ userKeysList, ...rest }) => {
                 ),
                 close,
                 submit,
-                onSubmit: rest.onClose
+                onSubmit: handleSubmit
             };
         }
 
