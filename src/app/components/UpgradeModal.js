5fe1ce5faeeadfb837fd649385304b49c1358796
edu <econdepe@protonmail.com>
2019-10-16 09:31:30 +0200

    pass ...rest to modal

--- src/app/components/UpgradeModal.js
+++ src/app/components/UpgradeModal.js
@@ -4,7 +4,7 @@ import { c } from 'ttag';
 import { ConfirmModal, Alert } from 'react-components';
 import { redirectTo } from 'proton-shared/lib/helpers/browser';
 
-const UpgradeModal = ({ onConfirm, onClose }) => {
+const UpgradeModal = ({ onConfirm, onClose, ...rest }) => {
     return (
         <ConfirmModal
             title={c('Title').t`Upgrade required`}
@@ -14,6 +14,7 @@ const UpgradeModal = ({ onConfirm, onClose }) => {
             }}
             onClose={onClose}
             confirm={c('Action').t`Upgrade`}
+            {...rest}
         >
             <Alert type="warning">{c('Warning').t`This feature requires a paid Proton account`}</Alert>
         </ConfirmModal>
