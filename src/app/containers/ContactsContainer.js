fe709e49c066e4f09cf417563e74504cba52d14a
Yiin <stanislovas.janonis@gmail.com>
2019-10-22 11:17:39 +0300

    Open storage link in the same window
    Fixes https://github.com/ProtonMail/react-components/issues/354

--- src/app/containers/ContactsContainer.js
+++ src/app/containers/ContactsContainer.js
@@ -193,7 +193,7 @@ const ContactsContainer = ({ location, history }) => {
             <AppsSidebar
                 items={[
                     <StorageSpaceStatus key="storage">
-                        <Href url="/settings/subscription" className="pm-button pm-button--primary">
+                        <Href url="/settings/subscription" target="_self" className="pm-button pm-button--primary">
                             {c('Action').t`Upgrade`}
                         </Href>
                     </StorageSpaceStatus>
