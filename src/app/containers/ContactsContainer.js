ea81c3b0d861d555b2ab61b8ad0ffe27fc4afc29
Richard <EpokK@users.noreply.github.com>
2019-10-25 08:58:01 +0200

    Fix #204 - Add title for ProtonContacts (#262)
    * Fix #204 - Add title for ProtonContacts
    
    * Review

--- src/app/containers/ContactsContainer.js
+++ src/app/containers/ContactsContainer.js
@@ -58,6 +58,10 @@ const ContactsContainer = ({ location, history }) => {
         return params.get('contactGroupID');
     }, [location.search]);
 
+    const contactGroup = useMemo(() => {
+        return contactGroups.find(({ ID }) => ID === contactGroupID);
+    }, [contactGroupID]);
+
     const hasChecked = useMemo(() => {
         return Object.keys(checkedContacts).some((key) => checkedContacts[key]);
     }, [checkedContacts]);
@@ -250,7 +254,7 @@ const ContactsContainer = ({ location, history }) => {
     );
 
     return (
-        <PrivateLayout>
+        <PrivateLayout title={contactGroup ? `${contactGroup.Name}` : c('Title').t`Contacts`}>
             {(!isNarrow || !contactID) && (
                 <PrivateHeader
                     title={c('Title').t`Contacts`}
--- src/app/containers/SettingsContainer.js
+++ src/app/containers/SettingsContainer.js
@@ -31,7 +31,7 @@ const SettingsContainer = ({ location }) => {
     ];
 
     return (
-        <PrivateLayout>
+        <PrivateLayout title={c('Title').t`Contact settings`}>
             <PrivateHeader
                 inSettings={true}
                 title={c('Title').t`Settings`}
