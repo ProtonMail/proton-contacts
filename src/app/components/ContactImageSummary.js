72b9ef7302d34be3c2fe9914a5a52bd9282fdd64
edu san <33841139+econdepe@users.noreply.github.com>
2019-11-08 17:15:17 +0100

    Prevent phantom requests when clicking on contacts with no image (#315)
    * prevent toImage(undefined)
    
    * load photo only when requested

--- src/app/components/ContactImageSummary.js
+++ src/app/components/ContactImageSummary.js
@@ -5,6 +5,7 @@ import { useMailSettings, useLoading, Loader, Button } from 'react-components';
 import { getInitial } from 'proton-shared/lib/helpers/string';
 import { isURL } from 'proton-shared/lib/helpers/validators';
 import { resizeImage, toImage } from 'proton-shared/lib/helpers/image';
+import { noop } from 'proton-shared/lib/helpers/function';
 import { SHOW_IMAGES } from 'proton-shared/lib/constants';
 import { CONTACT_IMG_SIZE } from '../constants';
 
@@ -14,8 +15,12 @@ const ContactImageSummary = ({ photo, name }) => {
     const [{ ShowImages }, loadingMailSettings] = useMailSettings();
     const [loadingResize, withLoadingResize] = useLoading();
     const loading = loadingMailSettings && loadingResize;
+    const showPhoto = ShowImages & SHOW_IMAGES.REMOTE || showAnyways;
 
     useEffect(() => {
+        if (!photo || !showPhoto) {
+            return;
+        }
         const resize = async () => {
             const { width, height } = await toImage(photo);
             setImage((image) => ({ ...image, width, height }));
@@ -31,8 +36,8 @@ const ContactImageSummary = ({ photo, name }) => {
             });
             setImage((image) => ({ ...image, src: resized }));
         };
-        withLoadingResize(resize());
-    }, [photo]);
+        withLoadingResize(resize().catch(noop));
+    }, [photo, showPhoto]);
 
     if (!photo) {
         return (
@@ -46,7 +51,7 @@ const ContactImageSummary = ({ photo, name }) => {
 
     const handleClick = () => setShowAnyways(true);
 
-    if (ShowImages & SHOW_IMAGES.REMOTE || showAnyways) {
+    if (showPhoto) {
         if (loading) {
             return <Loader />;
         }
