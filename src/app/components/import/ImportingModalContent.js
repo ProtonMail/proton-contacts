2b2c4b2a7710e7c782d63e2555d8e12c40176bc9
edu <econdepe@protonmail.com>
2019-11-08 10:26:43 +0100

    handle constants better

--- src/app/components/import/ImportingModalContent.js
+++ src/app/components/import/ImportingModalContent.js
@@ -13,12 +13,19 @@ import { extractVcards, parse as parseVcard } from '../../helpers/vcard';
 import { prepareContact } from '../../helpers/encrypt';
 import { splitContacts } from '../../helpers/import';
 import { combineProgress } from '../../helpers/progress';
-import { OVERWRITE, CATEGORIES, API_SAFE_INTERVAL, ADD_CONTACTS_MAX_SIZE } from '../../constants';
+import {
+    OVERWRITE,
+    CATEGORIES,
+    API_SAFE_INTERVAL,
+    ADD_CONTACTS_MAX_SIZE,
+    MAX_SIMULTANEOUS_CONTACTS_ENCRYPT
+} from '../../constants';
 import { API_CODES } from 'proton-shared/lib/constants';
 
 const { OVERWRITE_CONTACT } = OVERWRITE;
 const { IGNORE, INCLUDE } = CATEGORIES;
 const { SINGLE_SUCCESS } = API_CODES;
+const ONE_HOUR = 1000 * 60 * 60;
 
 const createParseErrorMessage = (index, message) =>
     c('Info on errors importing contacts').t`Contact ${index} from your list could not be parsed. ${message}`;
@@ -109,8 +116,8 @@ const ImportingModalContent = ({ isVcf, file = '', vcardContacts, privateKey, on
         const encryptContacts = async (contacts = [], { signal }) => {
             const publicKey = privateKey.toPublic();
 
-            // encrypt 5 contacts at a time to speed up the process without burning the user's machine
-            const contactBatches = chunk(contacts, 5);
+            // encrypt several contacts at a time to speed up the process without burning the user's machine
+            const contactBatches = chunk(contacts, MAX_SIMULTANEOUS_CONTACTS_ENCRYPT);
 
             const encryptedContacts = [];
             for (const batch of contactBatches) {
@@ -134,14 +141,14 @@ const ImportingModalContent = ({ isVcf, file = '', vcardContacts, privateKey, on
 
             const indexMap = contacts.map(({ index }) => index);
 
-            const INFINITE_TIME = 1000 * 60 * 60; // 1 hour
             const responses = (await apiWithAbort(
                 addContacts({
                     Contacts: contacts.map(({ contact }) => contact),
                     Overwrite: OVERWRITE_CONTACT,
                     Labels: labels,
-                    // we need to increase the standard timeout limit for this route since it seems a bit slow currently
-                    timeout: INFINITE_TIME
+                    // we need to increase the standard timeout limit for this route since it may be slow sometimes
+                    // 1 hour to wait for an API response is essentially infinite time
+                    timeout: ONE_HOUR
                 })
             )).Responses.map(({ Response }) => Response);
 
