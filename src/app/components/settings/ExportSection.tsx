40e12e7d3f6052a8faf5e87702d8af1d39f7b267
dhoko <dhoko@pm.me>
2020-12-09 17:31:16 +0100

    Improve string when we export contacts

--- src/app/components/settings/ExportSection.tsx
+++ src/app/components/settings/ExportSection.tsx
@@ -11,7 +11,7 @@ const ExportSection = () => {
         <>
             <Alert>
                 {c('Info')
-                    .t`The application needs to locally decrypt your contacts before you can export them. At the end of the process, a VCF file will be generated and you will be able to download it.`}
+                    .t`The application needs to locally decrypt your contacts before they can be exported. At the end of the process, a VCF file will be generated and you will be able to download it.`}
             </Alert>
             <div className="mb1">
                 <PrimaryButton onClick={handleExport} disabled={loadingUserKeys}>
